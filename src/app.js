6d0891d9d1d6148ad48459408f504068fc0ec14b
cinziamagnani <cinziamagnani@users.noreply.github.com>
2020-12-20 21:26:05 +0100

    update date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day = days[date.getDay()];
+
+    let datum = date.getDate();
+
+    let months = ['January','February','March','April','May','June','July','August','Septemeber','October','November','December'];
+    let month = months[date.getMonth()];
+
+    return `${hours}:${minutes}, ${day} ${datum} ${month}`;
+}
+
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
@@ -5,6 +28,7 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let descriptionElement = document.querySelector("#description");
+    let datetimeElement = document.querySelector("#datetime");
 
     cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -12,6 +36,7 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     descriptionElement.innerHTML = response.data.weather[0].description;
+    datetimeElement.innerHTML = formatDate(response.data.dt*1000);
 }
 
 let city = "Amsterdam";
